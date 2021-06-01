28065cf479496520ff3132b5f30d9a10e8798e78
Musthafa Faishal <musthafafaishal@gmail.com>
2021-06-02 03:46:54 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const submitBookForm = document.getElementById("inputBook");
+    const submitBookSearch = document.getElementById("searchBook");
+
+    submitBookForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        addBook();
+    });
+
+    submitBookSearch.addEventListener("submit", (event) => {
+        event.preventDefault();
+        searchBook();
+    });
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+    }
+});
+
+document.addEventListener("ondatasaved", () => {
+    console.log("Data berhasil disimpan.");
+});
+
+document.addEventListener("ondataloaded", () => {
+    refreshDataFromBookshelf();
+});
